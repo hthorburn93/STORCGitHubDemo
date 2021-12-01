c4c9a82f34c5bb916998f45ab37f4fb847eac7cc
Thorburn <thorburh@lancaster.ac.uk>
2021-12-01 10:14:36 +0000

    Create RollCall.py
    Created RollCall file

---
+++ RollCall.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 10:13:14 2021
+
+@author: thorburh
+"""
+
+
+print("Hello, my name is Hamish")
+
+Attendees=['Hamish','Ben','Dan']
+
+print("Welcome:")
+for i in Attendees:
+    print(i)
